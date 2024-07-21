a705de300127122be55e42e1d35f2bc54905afbf
Nguyễn Huỳnh Gia Phát <97014731+NguyenHuynhGiaPhat@users.noreply.github.com>
2024-07-21 17:12:43 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "lunoc.dks6u0b.mongodb.net",
+  DB_USER: "lunoc15122024",
+  DB_PASS: "WnG6nG3dB3FRKisb",
+  DB_DATABASE: "bookhaven",
+  JWT_SECRET: "khoabimat",
+  JWT_EXPIRES: "360000000", // in milliseconds
+  EMAIL_USER: "lunoc15122024@gmail.com", // gmail service
+  EMAIL_PASS: "adlwymxqmjkfatjc ",
+};
+module.exports = MyConstants;
