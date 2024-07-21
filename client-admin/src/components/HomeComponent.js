444709259d4d587d4b6f97fa5211e498b0affbff
Nguyễn Huỳnh Gia Phát <97014731+NguyenHuynhGiaPhat@users.noreply.github.com>
2024-07-21 15:39:33 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img
+          src="https://i.pinimg.com/originals/0d/cd/6f/0dcd6f4e410a34465a2d611913199e50.gif"
+          width="800px"
+          height="600px"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+export default Home;
